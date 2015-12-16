2879180128a7ce0a445574d548d202528a5da997
Anna Rankin <anna@annarankin.com>
2015-12-16 17:47:44 -0500

    adds css & classes for brackets

--- main.js
+++ main.js
@@ -32,84 +32,14 @@ var exampleObj2 = {
 }
 
 
-// // Function to display a JSON object, line-by-line in console
-// var displayObject = function displayObject(obj, parent, offset) {
-//   var parent = parent || "0";
-//   var offset = offset || " ";
-
-//   // If the "obj" argument is not an object, just display it as a key/value pair.
-//   if (typeof obj != "object") {
-
-//     console.log(offset + parent + ': ' + obj);
-
-//     // Otherwise, if it's an array, indent and loop through it, 
-//     // displaying everything inside. 
-//   } else if (Array.isArray(obj)) {
-//     console.log(offset + parent, '[');
-//     offset += "  "
-//     obj.forEach(function(el, idx) {
-//       displayObject(el, idx, offset);
-//     });
-//     offset = offset.slice(0, -2)
-//     console.log(offset + "],");
-
-//     // If obj is actually a JS object, display each key-value pair.
-//   } else if (typeof obj === "object") {
-//     console.log(offset + parent + ': {');
-//     offset += "  "
-
-//     var keys = Object.keys(obj);
-
-//     keys.forEach(function(key) {
-//       displayObject(obj[key], key, offset);
-//     });
-//     offset = offset.slice(0, -2)
-
-//     console.log(offset + "}");
-
-//     // If none of the above is true...
-//   } else {
-//     console.log("What")
-//   }
-// }
-
-
-// var displayObject = function(obj) {
-//   // get all keys/values at first level
-//   var firstLevelKeys = Object.keys(obj);
-//   var level = 1;
-//   // loop through keys, creating a p tag for each
-//   firstLevelKeys.forEach(function(keyName) {
-//     var $keyValPair = $('<p>');
-//     $keyValPair.addClass('level-' + level);
-
-//     var $key = $('<span class="key">');
-//     $key.data('name', keyName);
-//     $key.text(keyName);
-//     console.log($key);
-
-//     var $pairValue = $('<span class="value">');
-//     $pairValue.data('name', obj[keyName]);
-//     $pairValue.text(obj[keyName]);
-
-//     $keyValPair.append($key);
-//     $keyValPair.append(" : ");
-//     $keyValPair.append($pairValue);
-//     $(document.body).append($keyValPair);
-//     $(document.body).append("<br>");
-//   });
-// };
-
-
-// Function to display a JSON object, line-by-line on the DOM
-
-
-var displayObjectDom = function displayObjectDom(parent, offset, top, counter, $target) {
-  var parent = parent || "0";
+// Function 
+var displayObjectDom = function displayObjectDom($target, parent, offset, top, counter) {
+  var parent = parent || "root";
   var offset = offset || 10;
   var top = top || 10;
   var counter = counter || 0;
   var $target = $target || $(document.body);
+  var lineHeight = 25;
 
   var displayObject = function(obj, parent) {
     // If the "obj" argument is not an object, just display it as a key/value pair.
@@ -125,15 +55,15 @@ var displayObjectDom = function displayObjectDom(parent, offset, top, counter, $
         left: offset
       });
       $target.append($displayParagraph);
-      // Otherwise, if it's an array, indent and loop through it, 
-      // displaying everything inside. 
+    // Otherwise, if it's an array, indent and loop through it, 
+    // displaying everything inside. 
     } else if (Array.isArray(obj)) {
 
       var bracketId = counter;
       counter++;
 
       var $beginBracket = $('<p>');
-      $beginBracket.html("<span class='key'>" + parent + '</span>: [' + bracketId);
+      $beginBracket.html("<span class='key'>" + parent + '</span>: <span class="bracket ' + bracketId + '">[</span>');
       $beginBracket.css({
         position: "absolute"
       });
@@ -143,35 +73,34 @@ var displayObjectDom = function displayObjectDom(parent, offset, top, counter, $
       });
       console.log($beginBracket);
       $target.append($beginBracket);
-      offset += 20;
-      top += 20;
+      offset += lineHeight;
+      top += lineHeight;
 
       obj.forEach(function(el, idx) {
         displayObject(el, idx);
       });
 
       var $endBracket = $('<p>');
-      $endBracket.text("],");
+      $endBracket.html('<span class="bracket ' + bracketId + '">]</span>');
       $endBracket.css({
         position: "absolute"
       });
       $endBracket.offset({
         top: top,
-        left: offset
+        left: offset - lineHeight
       });
 
       console.log($endBracket);
       $target.append($endBracket);
-      offset -= 20;
+      offset -= lineHeight;
 
-      // If obj is actually a JS object, display each key-value pair.
+    // If obj is actually a JS object, display each key-value pair.
     } else if (typeof obj === "object") {
       var bracketId = counter;
       counter++;
 
-      // console.log(offset + parent + ': {');
       var $beginBracket = $('<p>');
-      $beginBracket.html("<span class='key'>" + parent + '</span>: {' + bracketId);
+      $beginBracket.html("<span class='key'>" + parent + '</span>: <span class="bracket ' + bracketId + '">{</span>');
       $beginBracket.css({
         position: "absolute"
       });
@@ -181,40 +110,40 @@ var displayObjectDom = function displayObjectDom(parent, offset, top, counter, $
       });
       console.log($beginBracket);
       $target.append($beginBracket);
-      offset += 20;
-      top += 20;
+      offset += lineHeight;
+      top += lineHeight;
 
       var keys = Object.keys(obj);
 
       keys.forEach(function(key) {
         displayObject(obj[key], key);
       });
-      // console.log(offset + "}");
       var $endBracket = $('<p>');
-      $endBracket.text("}," + bracketId);
+      $endBracket.html('<span class="bracket ' + bracketId + '">}</span>');
       $endBracket.css({
         position: "absolute"
       });
       $endBracket.offset({
         top: top,
-        left: offset
+        left: offset - lineHeight
       });
       console.log($endBracket);
       $target.append($endBracket);
-      offset -= 20;
+      offset -= lineHeight;
     }
-    top += 20;
+    top += lineHeight;
   };
   return displayObject;
 }
 
-
+var addBracketPairListeners = function() {};
 
 $(document).ready(function() {
   $('button').on('click', function() {
     objToDisplay = JSON.parse($('#json-input').val());
     console.log(objToDisplay);
-    displayObjectDom(0, 10, 10, 0, $('.results'))(objToDisplay);
+    displayObjectDom($('.results'))(objToDisplay, "root");
+    addBracketPairListeners();
   })
   // displayObjectDom()(exampleObj2);
 
